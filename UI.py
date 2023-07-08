4969bf542e9502c38132a10b0321f7ed706335a1
Laura Porsch <laupor112@gmail.com>
2023-07-08 10:02:23 -0600

    add docstrings, change UI by adding empty columns, add function calculate_time

--- UI.py
+++ UI.py
@@ -1,5 +1,9 @@
 from tkinter import *
-from colors import *
+
+LIGHTPINK = "#FCE9F1"
+DARKPINK = "#F1D4E5"
+TEAL = "#73BBC9"
+BLACK = "#080202"
 
 
 class WriteOrDye(Tk):
@@ -14,35 +18,44 @@ class WriteOrDye(Tk):
 
         self.title_label = Label()
         self.title_label.config(text="Write Or Dye", font=("Helvetica", 50, "bold"), fg=BLACK, bg=DARKPINK, pady=20)
-        self.title_label.grid(column=1, row=0, columnspan=3)
+        self.title_label.grid(column=3, row=0, columnspan=3)
 
         self.explanation = Label()
         self.explanation.config(text="If you stop writing for more than 5 seconds, all your progress will be lost.\n So"
                                      " don't give up!\n", font=("Helvetica", 10), fg=BLACK, bg=DARKPINK, pady=20)
-        self.explanation.grid(column=1, row=1, columnspan=3)
+        self.explanation.grid(column=3, row=1, columnspan=3)
+
+        self.empty_column_1 = Label(text="", bg=DARKPINK, width=10)
+        self.empty_column_1.grid(column=0, row=0, rowspan=5)
+        self.empty_column_2 = Label(text="", bg=DARKPINK, width=10)
+        self.empty_column_2.grid(column=2, row=0, rowspan=5)
+        self.empty_column_3 = Label(text="", bg=DARKPINK, width=10)
+        self.empty_column_3.grid(column=6, row=0, rowspan=5)
 
 
 class UserWriting(Text):
+    """creates a TextBox on the User Interface"""
     def __init__(self):
         super().__init__()
         self.config(width=51, height=5, font=("Helvetica", 20), bg=LIGHTPINK, pady=20)
-        self.grid(column=1, row=2, columnspan=3)
+        self.grid(column=3, row=2, columnspan=3)
 
     def end_app(self):
+        """deletes the content of the TextBox"""
         self.delete("1.0", END)
 
 
 class StartButton(Button):
+    """creates a Button on the User Interface"""
     def __init__(self):
         super().__init__()
         self.config(text="START", font=("Helvetica", 25, "bold"), bg=TEAL, fg=BLACK)
-        self.grid(column=0, row=3)
+        self.grid(column=1, row=3)
 
 
 class Timer(Label):
+    """creates a Label on the User Interface"""
     def __init__(self):
         super().__init__()
         self.config(text="5 secs", font=("Helvetica", 30, "bold"), bg=TEAL, fg=BLACK,)
-        self.grid(column=4, row=3)
-
-
+        self.grid(column=7, row=3)
--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-from tkinter import *
 from UI import WriteOrDye, UserWriting, StartButton, Timer
+import time
+from tkinter import messagebox
 
 countdown = None
+start_time = 0
+end_time = 0
 
 
 def start_app(event):
-    """triggers the start of app"""
+    """focuses the cursor to the TextBox and calls function start_timer"""
+    global start_time
     writing.focus()
     start_timer(5)
+    start_time = time.time()
 
 
 def start_timer(count):
+    """starts timer with the set amount 'count', while timer is running, refreshes window every second to create
+    impression of a running countdown, when count is 0, calls function end_app"""
     global countdown
     if count >= 0:
         timer.config(text=f"{count} secs")
         countdown = window.after(1000, start_timer, count - 1)
     else:
         writing.end_app()
+        calculate_time()
 
 
 def is_writing(event):
+    """cancels the running countdown, resets the timer by resetting the displayed text, calls function start_timer"""
     global countdown
     window.after_cancel(countdown)
     timer.config(text="5 secs")
     start_timer(5)
+
+
+def calculate_time():
+    """calculates time between call of start_app and end_app, creates pop-up window that shows runtime, resets start
+    and end time"""
+    global start_time, end_time
+    end_time = time.time()
+    run_time = round((end_time - start_time)/60)
+    messagebox.showinfo(title="Well done!", message=f"You were writing for {run_time} min")
+    start_time = 0
+    end_time = 0
 
 
 window = WriteOrDye()
